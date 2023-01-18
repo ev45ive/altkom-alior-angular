82114c83507da255a5afcb28c1195cc9f69db228
Mateusz Kulesza <ev45ive@gmail.com>
2023-01-18 10:28:05 +0100

    Angular Root Component ( Custom HTML ) - @Component()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root, .placki[sos=malinowy]',
+  templateUrl: './app.component.html', // View
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'alior';
+  title = 'Alior';
 }
